b51c4dbf6b7d36a6c958c8f4913434b993cf489e
Горлов Максим <gorlov.maxim@gmail.com>
2015-09-17 03:11:41 +0300

    Модуль опций

---
+++ lib/Minecraft/Settings.pm
@@ -0,0 +1,57 @@
+package Minecraft::Settings;
+
+use strict;
+use warnings;
+use JSON;
+use Data::Dumper;
+use Exporter qw(import);
+use Time::HiRes qw (sleep);
+
+sub read_config
+{
+    my $c = {
+                'user'   => read_config_file('user-config.json'),
+                'system' => -e 'system-config.json' ? read_config_file('system-config.json') : {}
+            };
+    if(!exists($c->{'user'}{'paths'}{'temp'})) { $c->{'user'}{'paths'}{'temp'} = '/tmp'; }
+    if(!exists($c->{'user'}{'paths'}{'screenshosts'})) { $c->{'user'}{'paths'}{'screenshosts'} = './screenshots'; }
+    if(!exists($c->{'user'}{'minecraft'}{'title'})) { $c->{'user'}{'minecraft'}{'title'} = 'Minecraft 1.8.8'; }
+    if(!exists($c->{'user'}{'timeouts'}{'between_mouse_hide_and_screenshot'})) { $c->{'user'}{'timeouts'}{'between_mouse_hide_and_screenshot'} = 0.1; }
+    if(!exists($c->{'user'}{'timeouts'}{'villager_upgrade'})) { $c->{'user'}{'timeouts'}{'villager_upgrade'} = 5; }
+    if(!exists($c->{'user'}{'timeouts'}{'trade_interface_open'})) { $c->{'user'}{'timeouts'}{'trade_interface_open'} = 1; }
+    if(!exists($c->{'user'}{'timeouts'}{'max_trade_interface_open'})) { $c->{'user'}{'timeouts'}{'max_trade_interface_open'} = 15; }
+    if(!exists($c->{'user'}{'timeouts'}{'mouse_click_ms'})) { $c->{'user'}{'timeouts'}{'mouse_click_ms'} = 100; }
+    if(!exists($c->{'user'}{'minecraft'}{'texture_pack'})) { $c->{'user'}{'minecraft'}{'texture_pack'} = "default"; }
+    return $c;
+}
+
+sub read_config_file
+{
+    my $file_name = $_[0];
+    local $/;
+    open( my $fh, '<', $file_name );
+    my $json_text   = <$fh>;
+    my $perl_scalar = from_json($json_text, {utf8 => 1});
+    close($fh);
+    return $perl_scalar;
+}
+
+sub save_system_config
+{
+    my $config = $_[0];
+    open( my $fh, '>', 'system-config.json' );
+    print {$fh} to_json($config->{'system'}, {utf8 => 1, pretty => 1});
+    close($fh);
+    return read_config();
+}
+
+sub save_user_config
+{
+    my $config = $_[0];
+    open( my $fh, '>', 'user-config.json' );
+    print {$fh} to_json($config->{'user'}, {utf8 => 1, pretty => 1});
+    close($fh);
+    return read_config();
+}
+
+1;
